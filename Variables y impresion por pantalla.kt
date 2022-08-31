89569896a962fd1a2079912a50dcdd2c09c2a6d0
BastianPozo <102688920+BastianPozo@users.noreply.github.com>
2022-08-31 14:51:49 -0400

    Primera parte de fundamentos
    Primera parte : 
    Variable
    Impresión por pantalla

---
+++ Variables y impresion por pantalla.kt
@@ -0,0 +1,11 @@
+fun main() {
+	// "Var" para crear variables las variables son contenedores que guardan un datos
+	//que se le otorgan con un ejemplo de "Var = 1" o tambien con letras "Var ="A"
+	var Numero = 1
+	var Letras = "A"
+	// Esto es simplemente un contenedor 
+	println (Numero)
+    println (Letras)
+    //Los "Println" son para mostrar por pantalla un mensaje o una variable por ejemplo "println ("Hola Mundo")"
+    //Y por pantalla aparecera el "Hola Mundo"
+}
