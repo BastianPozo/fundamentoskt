859a03dbd36620c63817cd60f078c3e5ffab48b8
BastianPozo <102688920+BastianPozo@users.noreply.github.com>
2022-09-11 18:11:06 -0300

    Calculadora Básica
    Calculadora básica:
    Suma +
    Resta -
    Multiplicación *
    División /

---
+++ Calculadora Basica.kt
@@ -0,0 +1,42 @@
+fun main(){
+    
+    println("Calculadora Basica:")
+
+    println()
+    println()
+    
+    var num1 = 2
+    var num2 = 2
+    var resultado1 = (num1 + num2)
+    println ("El resultado de la suma es:")
+    println ("------------>" + resultado1 + "<------------")
+    
+    println()
+    println()
+    
+    var num3 = 2
+    var num4 = 2
+    var resultado2 = (num3 - num4)
+    println ("El resultado de la resta es:")
+    println ("------------>" + resultado2 + "<------------")
+    
+    println()
+    println()
+    
+    var num5 = 2
+    var num6 = 2
+    var resultado3 = (num5 * num6)
+    println ("El resultado de la multiplicación es:")
+    println ("------------>" + resultado3 + "<------------")
+    
+    
+    println()
+    println()
+   
+   
+    var num7 = 2
+    var num8 = 2
+    var resultado4 = (num7 / num8)
+    println ("El resultado de la división es:")
+    println ("------------>" + resultado4 + "<------------")
+}
