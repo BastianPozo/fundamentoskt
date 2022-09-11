e80f654cb6935f55f65227f60a614fc79f0ff37a
BastianPozo <102688920+BastianPozo@users.noreply.github.com>
2022-09-11 15:38:08 -0300

    Operadores Relacionales y Condicionales
    Segunda parte:
    Operadores Relacionales
    Condicional (if - else)

---
+++ Operadores logicos y condicional if - else.kt
@@ -0,0 +1,39 @@
+fun main(){
+    // Ejemplo usando if-else
+	println("¿Cuantos años tienes?")
+	var edad = 20
+    println (edad)
+	if (edad >= 18) {
+    	println("Puedes pasar")
+	} else {
+    	println("Lo siento, vuelve dentro de " + (18 - edad) + " años")	
+    }
+}
+
+
+// if (Condicional) {
+ 
+// } else {
+ 
+// acciones por falso, osea puedes dar a entender que la condision de "if" no se cumple 
+
+// }
+
+//       simbologia para usar en la condicional:
+             
+//                == Igual que
+//                != Diferente a / Distinto a
+//                 < Menor que
+//                 > Mayor que
+//                <= Menor igual que
+//                >= Mayor igual que
+
+
+
+
+
+
+
+
+
+//.
