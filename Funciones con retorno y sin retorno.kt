33c39df253673cf2c5710c7487fcbd459a1cd39d
BastianPozo <102688920+BastianPozo@users.noreply.github.com>
2022-09-18 17:40:11 -0300

    Funciones
    Funciones:
    Con Retorno 
    Sin retorno 
    Ejemplos

---
+++ Funciones con retorno y sin retorno.kt
@@ -0,0 +1,26 @@
+fun main() {
+	showMyName()
+    showMyLastName()
+    showMyAge()
+}
+fun showMyName(){
+    println("Me llamo Bastian")
+}
+fun showMyLastName(){
+    println("Mi Apellido es Pozo")
+}
+fun showMyAge(){
+    println("Tengo 19 años")
+}
+
+//funciones sin parametro de salida 
+
+
+
+
+
+fun Ejemplo(x: Int): Int {
+    return x * x
+}
+
+//ejemplo de funcion con retorno 
